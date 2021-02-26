a8d32b03b44d6d1e670e076d2d791dd29cb7bec8
James Sullivan <james.sullivan@live.co.uk>
2021-02-26 15:58:02 +0000

    Linking pizza and topping schemas. Adding toppings preview.

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -42,5 +42,36 @@ export default {
       validation: (Rule) => Rule.min(1000).max(50000),
       // TODO: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      // type topping is the name in topping.js
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      // this is diff from the toppings version name or title it can be anything
+      title: 'name',
+      media: 'image',
+      // gets the first 4 toppings
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    // fields.name and fields.vegetarian has been destructured
+    prepare: ({ title, media, ...toppings }) => {
+      // filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // return preview object for the pizza
+      return {
+        title,
+        media,
+        // Object.values() turns toppings object into an array to allow join to work
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
